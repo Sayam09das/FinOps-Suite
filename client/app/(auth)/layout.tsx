027647b0bfe4e66ca6462b8176bca1bdfc38fe20
Sayam09das <dassayam2021@gmail.com>
2026-04-23 10:22:23 +0530

    refactor(auth): simplify AuthLayout by removing unnecessary AppProvider wrapper

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -1,15 +1,7 @@
-import { AppProvider } from "../providers/app-provider"
-
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="relative flex min-h-full flex-col">
-      <AppProvider>
-        {children}
-      </AppProvider>
-    </div>
-  );
+  return <div className="relative flex min-h-full flex-col">{children}</div>;
 }
